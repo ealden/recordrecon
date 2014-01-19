9a50ff6792ee6ce595bacb6d85c48a380ba0e636
Ealden Esto E. Escañan <ealden@escanan.com>
2014-01-19 17:03:25 +0800

    Match name and processed_at between Record and TargetTransaction

--- lib/recordrecon/record_matcher.rb
+++ lib/recordrecon/record_matcher.rb
@@ -1,7 +1,35 @@
 module RecordRecon
   class RecordMatcher
     def self.match
-      []
+      records = Hash.new
+      targets = Hash.new
+
+      Record.where(:target_name => nil).each do |record|
+        key = {
+          :name => record.source_name,
+          :processed_at => record.tagged_at
+        }
+
+        records[key] = record
+      end
+
+      TargetTransaction.all.each do |target|
+        key = {
+          :name => target.name,
+          :processed_at => target.processed_at
+        }
+        targets[key] = target
+      end
+
+      results = []
+
+      records.each do |key, record|
+        if targets.has_key? key
+          results << [record.to_a, targets[key].to_a].flatten
+        end
+      end
+
+      results
     end
   end
 end
--- test/lib/recordrecon/record_matcher_test.rb
+++ test/lib/recordrecon/record_matcher_test.rb
@@ -3,8 +3,19 @@ require 'recordrecon/record_matcher'
 
 module RecordRecon
   class RecordMatcherTest < ActiveSupport::TestCase
+    fixtures :records, :target_transactions
+
     test 'match' do
-      assert_empty RecordMatcher.match
+      results = RecordMatcher.match
+
+      assert_equal 1, results.count
+
+      result = results[0]
+      assert_equal 15, result.count
+      assert_equal '1234567891', result[4]
+      assert_equal Date.parse('2014-01-08'), result[9]
+      assert_equal '1234567891', result[10]
+      assert_equal Date.parse('2014-01-08'), result[14]
     end
   end
 end
