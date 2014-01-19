ebe421581f8188795d6ecc00a6d33cea78cd5608
Ealden Esto E. Escañan <ealden@escanan.com>
2014-01-19 12:37:40 +0800

    Load records from Excel

--- lib/recordrecon/record_loader.rb
+++ lib/recordrecon/record_loader.rb
@@ -1,15 +1,27 @@
 module RecordRecon
   class RecordLoader
+    def self.import records
+      loader = RecordLoader.new
+
+      Spreadsheet.open(records).worksheet(0).each_with_index do |row, i|
+        next if i == 0
+
+        loader.import row
+      end
+    end
+
     def import record
       Record.create do |r|
         r.target_name = record[0]
         r.target_category = record[1]
-        r.target_count = record[2]
-        r.source_name = record[3]
-        r.source_category = record[4]
-        r.source_count = record[5]
-        r.tag = record[6]
-        r.tagged_at = record[7]
+        r.target_created_at = record[2]
+        r.target_count = record[3]
+        r.source_name = record[4]
+        r.source_category = record[5]
+        r.source_created_at = record[6]
+        r.source_count = record[7]
+        r.tag = record[8]
+        r.tagged_at = record[9]
       end
     end
   end
--- test/lib/recordrecon/record_loader_test.rb
+++ test/lib/recordrecon/record_loader_test.rb
@@ -5,11 +5,17 @@ module RecordRecon
   class RecordLoaderTest < ActiveSupport::TestCase
     test 'import' do
       loader = RecordLoader.new
-      record = [ '1234567890', 'ABC', 2, '1234567890', 'ABC', 2, 'GOOD', '2014-01-01' ]
+      record = [ '1234567890', 'ABC', '2014-01-01', 2, '1234567890', 'ABC', '2014-01-01', 2, 'GOOD', '2014-01-01' ]
 
       loader.import record
 
       assert_equal 1, Record.count
+    end
+
+    test 'import xls' do
+      RecordLoader.import 'test/assets/records.xls'
+
+      assert_equal 6, Record.count
     end
   end
 end
